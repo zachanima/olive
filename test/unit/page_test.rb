c19fe3bb1b281d8caae46237b4f1f8860ff26e2b
Zach Ewert <zachanima@gmail.com>
2011-08-02 04:27:59 +0200

    Test for not saving page with empty title

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class PageTest < ActiveSupport::TestCase
   test "should not save page without title" do
     page = Page.new
-    assert !page.save, "Saved page without title"
+    assert !page.save
+  end
+
+  test "should not save page with empty title" do
+    page = Page.new
+    page.title = ''
+    assert !page.save
   end
 end
