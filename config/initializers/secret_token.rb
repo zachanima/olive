cdd28ec342e619f18d6a988e89f8530d24755c49
Zach Ewert <zachanima@gmail.com>
2011-08-02 02:26:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Olive::Application.config.secret_token = '323654b87254de1ab46b04c2488f64199642b6f58dc87fcda5fdfad750affa2957061b1a71ed0fb9cb123421d48085679d5a12864b078679cd45bee1cc5761de'
