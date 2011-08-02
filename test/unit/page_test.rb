5dca5259a37fb40c289643d24e2f8b3091991a14
Zach Ewert <zachanima@gmail.com>
2011-08-02 03:53:05 +0200

    Validate, test for presence of title

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save page without title" do
+    page = Page.new
+    assert !page.save, "Saved page without title"
+  end
 end
