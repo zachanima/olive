97f1570a3621a8999d311f93eb0e144693a1df7d
Zach Ewert <zachanima@gmail.com>
2011-08-02 04:31:22 +0200

    Revert to should save in page unit test

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  test "not saving page without title" do
+  test "should not save page without title" do
     page = Page.new
     assert !page.save
   end
 
-  test "not saving page with empty title" do
+  test "should not save page with empty title" do
     page = Page.new
     page.title = ''
     assert !page.save
   end
 
-  test "saving page with non-empty title" do
+  test "should save page with non-empty title" do
     page = Page.new
     page.title = 'foo'
     assert page.save
